4464b0dc5aa7231009a580eb931d6ee3333580a9
sandeep sagar <samueldamien2011@gmail.com>
2019-06-09 17:42:48 +0530

    added new objects

---
+++ HERBONA/Model/ItemCategory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SmartTrucking.Model
+{
+    public class ItemCategorylistModel
+    {
+        public string ID { get; set; }
+        public string NAME { get; set; }
+        public string CODE { get; set; }
+        public string ImageURL { get; set; } 
+        public string IsActive { get; set; }
+    }
+    public class ItemCategoryModel
+    {
+        public string ID { get; set; }
+        public string NAME { get; set; }
+        public string CODE { get; set; }
+        public string ImageURL { get; set; }
+        public string IsActive { get; set; }
+    }
+}
