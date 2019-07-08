ac60de9c92e1971b0996d935dc9a7b14bf302362
Chandan Koley <chandan.koley@gmail.com>
2019-07-09 02:33:26 +0530

    order

--- HERBONA/ClientJS/OrderProcess.js
+++ HERBONA/ClientJS/OrderProcess.js
@@ -19,9 +19,165 @@ $(document).ready(function () {
             }
         }
     }
-    //alert(id);
+    setTimeout(function () {
+        if (id == undefined) {
+           
+        }
+        else {
+            alert(id);
+            $('#ID_hidden').val(id);
+            Req = 'Wallet_Balance@Payment_Details';
+            obj = "Fill";
+            url = "OrderProcess.aspx/PaymentDetails";
+            ht = {};
+            ht["Order_Details_id"] = $('#ID_hidden').val();
+            LoadAjaxPayment(ht, obj, Req, url);
+        }
+    }, 1000);
+});
+function LoadAjaxPayment(ht, obj, Req, url) {
+    $('body').pleaseWait();
+
+    $.ajax({
+        type: "POST",
+        url: url,
+        data: "{ht:" + JSON.stringify(ht) + ",Type :'" + obj + "' ,Req :'" + Req + "'}",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (Result) {
+            var json = jQuery.parseJSON(Result.d.ErrorDetail);
+            var sa_error = "";
+            var sa_errrorMsg = "";
+            $.each(json, function (index, K) {
+                sa_error = K.Error;
+                sa_errrorMsg = K.ErrorMessage;
+            });
+            if (sa_error != 'false') {
+                swal("", sa_errrorMsg, "error");
+                $('body').pleaseWait('stop');
+                return 0;
+            }
+
+            if (obj == "Fill") {
+
+                if (Result.d.Wallet_Balance != "" && Result.d.Wallet_Balance != undefined) {
+                    var json = jQuery.parseJSON(Result.d.Wallet_Balance);
+                    $.each(json, function (index, item) {
+                        $("#Div_Wallet_bal").html("<label id='LBL_Wallet_Balance'>&#x20b9; "+parseFloat(item.Wallet_Balance).toFixed(2)+"</label>");
+                        $("#Small_Total_Wallet_Amount").text('Available Balance :INR ' + parseFloat(item.Wallet_Balance).toFixed(2));
+                        $("#Total_Wallet_Balance_Hidden").val(parseFloat(item.Wallet_Balance).toFixed(2));
+                    });
+                }
+                if (Result.d.Payment_Details != "" && Result.d.Payment_Details != undefined) {
+                    var json = jQuery.parseJSON(Result.d.Payment_Details);
+                    $.each(json, function (index, item) {
+                        $("#H_Sales_Amount").text('Sales Amount :INR ' + parseFloat(item.SALES_AMOUNT).toFixed(2));
+                        $("#H_Shipping_Changes").text('Shipping Charges :INR ' + parseFloat(item.SHIPPING).toFixed(2));
+                        $("#H_Net_Amount").text(parseFloat(item.NET_AMOUNT).toFixed(2));
+                        $("#Net_Payable_Amount_Hidden").val(parseFloat(item.NET_AMOUNT).toFixed(2));
+                        $("#H_Balance_Amount").text('Balance Amount :INR ' + parseFloat(item.NET_AMOUNT).toFixed(2));
+                    });
+                }
+            }
+            $('body').pleaseWait('stop');
+        }
+    });
+}
+$('#chk_Wallet').on('ifChecked', function (event) {
+    $("#Txt_Wallet_Amount").prop("disabled", false);
+});
+$('#chk_Wallet').on('ifUnchecked', function (event) {
+    $("#Txt_Wallet_Amount").prop("disabled", true);
+    $('#Txt_Wallet_Amount').val("0.00");
+    Calculate_Balance();
+});
+
+$('#chk_Gateway').on('ifChecked', function (event) {
+    $("#Txt_Gateway_Amount").prop("disabled", false);
+});
+$('#chk_Gateway').on('ifUnchecked', function (event) {
+    $("#Txt_Gateway_Amount").prop("disabled", true);
+    $('#Txt_Gateway_Amount').val("0.00");
+    Calculate_Balance();
+});
+
+$('#chk_Cash').on('ifChecked', function (event) {
+    $("#txt_Cash_Amount").prop("disabled", false);
+});
+$('#chk_Cash').on('ifUnchecked', function (event) {
+    $("#txt_Cash_Amount").prop("disabled", true);
+    $('#txt_Cash_Amount').val("0.00");
+    Calculate_Balance();
 });
 $('input[type="checkbox"].flat-red, input[type="radio"].flat-red').iCheck({
     checkboxClass: 'icheckbox_flat-green',
     radioClass: 'iradio_flat-green',
 });
+
+function Calculate_Balance()
+{
+    var text_Wallet, text_getway, text_Cash;
+    if ($('#Txt_Wallet_Amount').val() == undefined || $('#Txt_Wallet_Amount').val() == "") {
+        $('#Txt_Wallet_Amount').val("0.00");
+        text_Wallet = 0;
+    }
+    else
+    {
+        if ((parseFloat($('#Txt_Wallet_Amount').val()).toFixed(2)) > (parseFloat($('#Total_Wallet_Balance_Hidden').val()).toFixed(2))) {
+            popupErrorMsg($("#Txt_Wallet_Amount"), "You dont have enough amount in Wallet .", 5);
+            $('#Txt_Wallet_Amount').val("0.00");
+            text_Wallet =0;
+        }
+        else {
+            $('#Txt_Wallet_Amount').val(parseFloat($('#Txt_Wallet_Amount').val()).toFixed(2));
+            text_Wallet = parseFloat($('#Txt_Wallet_Amount').val()).toFixed(2);
+        }
+    }
+    if ($('#Txt_Gateway_Amount').val() == undefined || $('#Txt_Gateway_Amount').val() == "") {
+        $('#Txt_Gateway_Amount').val("0.00");
+        text_getway = 0;
+    }
+    else
+    {
+        $('#Txt_Gateway_Amount').val(parseFloat($('#Txt_Gateway_Amount').val()).toFixed(2));
+        text_getway = parseFloat($('#Txt_Gateway_Amount').val()).toFixed(2);
+    }
+    if ($('#txt_Cash_Amount').val() == undefined || $('#txt_Cash_Amount').val() == "") {
+        $('#txt_Cash_Amount').val("0.00");
+        text_Cash = 0;
+    }
+    else {
+        $('#txt_Cash_Amount').val(parseFloat($('#txt_Cash_Amount').val()).toFixed(2));
+        text_Cash = parseFloat($('#txt_Cash_Amount').val()).toFixed(2);
+    }
+
+    /// Balance calculation
+    var Total_payment_amount = parseFloat(text_Wallet) + parseFloat(text_getway) + parseFloat(text_Cash);
+    var Total_Payable_amount = (parseFloat($('#Net_Payable_Amount_Hidden').val()).toFixed(2));
+    $('#H_Balance_Amount').text('Balance Amount :INR ' + parseFloat(parseFloat(Total_Payable_amount) - parseFloat(Total_payment_amount)).toFixed(2));
+    //alert(Total_payment_amount);
+    if (parseFloat(Total_payment_amount) == parseFloat(Total_Payable_amount))
+    {
+        $('#divproceed').show();
+    }
+    else
+        if(parseFloat(Total_payment_amount) < parseFloat(Total_Payable_amount))
+        {
+            $('#divproceed').hide();
+        }
+        else
+            if (parseFloat(Total_payment_amount) > parseFloat(Total_Payable_amount)) {
+                swal({
+                    title: "Amount Can't be exceed",
+                    confirmButtonColor: "#5cb85c",
+                    confirmButtonText: "Ok!",
+                    closeOnConfirm: true,
+                    timer: 2000
+                });
+                $('#divproceed').hide();
+            }
+}
+function finalProceed()
+{
+
+}
--- HERBONA/OrderProcess.aspx.cs
+++ HERBONA/OrderProcess.aspx.cs
@@ -21,9 +21,11 @@ namespace SmartTrucking
         public static DataSet ds = new DataSet();
 
         public static JavaScriptSerializer serializer = new JavaScriptSerializer();
-        static List<CountryModel> _CountryModel = new List<CountryModel>();
-        static List<ErrorModel> _errorDetail = new List<ErrorModel>();
-      
+        static List<ErrorModel> _ErrorModel = new List<ErrorModel>();
+        static List<UserSaveModel> _UserSaveModel = new List<UserSaveModel>();
+        static List<UserSaveModelWithID> _UserSaveModelWithID = new List<UserSaveModelWithID>();
+        static List<Wallet_balanceModel> _Wallet_balanceModel = new List<Wallet_balanceModel>();
+        static List<Payment_DetailsModel> _Payment_DetailsModel = new List<Payment_DetailsModel>();
         protected void Page_Load(object sender, EventArgs e)
         {
             if (HttpContext.Current.Session["Company_ID"] == null || HttpContext.Current.Session["Company_ID"] == "")
@@ -33,6 +35,92 @@ namespace SmartTrucking
             }
 
         }
-      
+
+        [System.Web.Services.WebMethod]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+        public static Hashtable PaymentDetails(Hashtable ht, string Type, string Req)
+        {
+            try
+            {
+                string[] words = null;
+                string Data = Req;
+
+                ReturnData.Clear();
+
+                if (Type == "Fill")
+                {
+                    words = null;
+                    words = Req.Split('@');
+                    for (int m = 0; words.Count() > m; m++)
+                    {
+
+                        Data = words[m].ToString();
+
+                      
+                        if (Data == "Wallet_Balance")
+                        {
+                            _Wallet_balanceModel.Clear();
+                            ht_param.Clear();
+                            ht_param.Add("@Order_Details_id", ht["Order_Details_id"].ToString());
+                            ds = db.SysFetchDataInDataSet("[GET_MEMBER_WALLET_BALANCE]", ht_param);
+
+                            if (ds.Tables.Count > 0)
+                            {
+                                _Wallet_balanceModel = ds.Tables[0].AsEnumerable()
+                                  .Select(row => new Wallet_balanceModel
+                                  {
+                                      Wallet_Balance = row["Wallet_Balance"].ToString()
+                                  }).ToList();
+                            }
+                            ReturnData["Wallet_Balance"] = serializer.Serialize(_Wallet_balanceModel);
+                        }
+
+                        if (Data == "Payment_Details")
+                        {
+                            _Payment_DetailsModel.Clear();
+                            ht_param.Clear();
+                            ht_param.Add("@Order_Details_id", ht["Order_Details_id"].ToString());
+                            ds = db.SysFetchDataInDataSet("[GET_MEMBER_SHIPPING]", ht_param);
+
+                            if (ds.Tables.Count > 0)
+                            {
+                                _Payment_DetailsModel = ds.Tables[0].AsEnumerable()
+                                  .Select(row => new Payment_DetailsModel
+                                  {
+                                      SALES_AMOUNT = row["SALES_AMOUNT"].ToString(),
+                                      SHIPPING = row["SHIPPING"].ToString(),
+                                      NET_AMOUNT = row["NET_AMOUNT"].ToString()
+                                  }).ToList();
+                            }
+                            ReturnData["Payment_Details"] = serializer.Serialize(_Payment_DetailsModel);
+                        }
+
+                    }
+                }
+
+                _ErrorModel.Clear();
+                ErrorModel _error = new ErrorModel();
+                _error.Error = "false";
+                _error.ErrorMessage = "sucess";
+                _ErrorModel.Add(_error);
+                ReturnData["ErrorDetail"] = serializer.Serialize(_ErrorModel);
+                HttpContext.Current.Response.AppendHeader("ResponseHeader", "200");
+
+            }
+            catch (Exception ex)
+            {
+                _ErrorModel.Clear();
+                ReturnData.Clear();
+                ErrorModel _error = new ErrorModel();
+                _error.Error = "true";
+                _error.ErrorMessage = "Some problem occurred please try again later";
+                _ErrorModel.Add(_error);
+                ReturnData["ErrorDetail"] = serializer.Serialize(_ErrorModel);
+                HttpContext.Current.Response.AppendHeader("ResponseHeader", "500");
+            }
+            return ReturnData;
+        }
+
+
     }
 }
